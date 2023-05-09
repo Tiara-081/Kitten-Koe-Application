3aa1e99ac0b92482fe594dfe77b7dac56fc3efe7
Tiara-081 <tiaraaara12@gmail.com>
2023-05-10 02:10:53 +0700

    first commit

---
+++ app/src/main/java/com/kittenecho/Konten_App.java
@@ -0,0 +1,15 @@
+package com.kittenecho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Konten_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kontent_app);
+    }
+}
+
